9e6349ffcce717ac97fc4a7492b4d4cd1b3f9d16
Gabriel Kzk <gabriel.nasimeto34@gmail.com>
2026-06-09 20:08:27 -0300

    Criação do escopo do angular

---
+++ src/app/snake/components/game-over/game-over.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-over',
+  imports: [],
+  templateUrl: './game-over.html',
+  styleUrl: './game-over.scss',
+})
+export class GameOver {}
---
+++ src/app/snake/components/score-board/score-board.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-score-board',
+  imports: [],
+  templateUrl: './score-board.html',
+  styleUrl: './score-board.scss',
+})
+export class ScoreBoard {}
