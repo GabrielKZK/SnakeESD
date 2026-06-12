63d2ae3ba9a60d0ed29ba80999a6d3d5204a6f03
FELIPE CASCAES FERNANDES <felipe_c_fernandes@estudante.sesisenai.org.br>
2026-06-11 21:17:27 -0300

    components

--- src/app/snake/components/game-over/game-over.ts
+++ src/app/snake/components/game-over/game-over.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-game-over',
-  imports: [],
   templateUrl: './game-over.html',
-  styleUrl: './game-over.scss',
+  styleUrls: ['./game-over.scss']
 })
-export class GameOver {}
+export class GameOverComponent {
+  @Input() finalScore: number = 0;
+  @Output() onRetry = new EventEmitter<void>();
+
+  public retry(): void {
+    this.onRetry.emit();
+  }
+}
