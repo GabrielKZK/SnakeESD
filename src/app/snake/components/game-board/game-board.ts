90fa743249ed9169ea544ad09ff0e84880ebc083
FELIPE CASCAES FERNANDES <felipe_c_fernandes@estudante.sesisenai.org.br>
2026-06-16 21:23:14 -0300

    Game-board atualizado

--- src/app/snake/components/game-board/game-board.ts
+++ src/app/snake/components/game-board/game-board.ts
@@ -10,4 +10,13 @@ import { CommonModule } from '@angular/common';
 })
 export class GameBoardComponent {
   @Input() matriz: number[][] = [];
+  @Input() direction: { x: number, y: number } = { x: 1, y: 0 };
+
+  // Função para girar a cabeça do triângulo dependendo de onde a cobra vai
+  getHeadRotation(): string {
+    if (this.direction.x === 1) return 'rotate(90deg)';   // Direita
+    if (this.direction.x === -1) return 'rotate(-90deg)'; // Esquerda
+    if (this.direction.y === 1) return 'rotate(180deg)';  // Baixo
+    return 'rotate(0deg)';                                // Cima
+  }
 }
